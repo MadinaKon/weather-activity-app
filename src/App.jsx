2d1590ee39d1db7282cbc0704b89703b62dec114
MadinaKon <141622998+MadinaKon@users.noreply.github.com>
2023-09-18 17:58:15 +0200

    add Fetch weather API

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,38 @@ import Form from "./components/Form/Form";
 import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
 import List from "./components/List/List";
+import { useEffect, useState } from "react";
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: [],
   });
-  const isGoodWeather = true;
+
+  const [weather, setWeather] = useState("");
+
+  async function fetchWeatherApi() {
+    const response = await fetch("https://example-apis.vercel.app/api/weather");
+    const data = await response.json();
+    setWeather(data);
+  }
+
+  useEffect(() => {
+    fetchWeatherApi();
+  }, []);
 
   function handleAddActivity(newActivity) {
     setActivities([...activities, { ...newActivity, id: uid() }]);
   }
 
   const filteredActivities = activities.filter(
-    (activity) => activity.isForGoodWeather === isGoodWeather
+    (activity) => activity.isForGoodWeather === weather
   );
 
   return (
     <>
-      <List activities={filteredActivities} isGoodWeather={isGoodWeather} />
+      <h2> Condition {weather.condition}</h2>
+      <h3>Temperature {weather.temperature}</h3>
+      <List activities={filteredActivities} isGoodWeather={weather} />
       <Form onAddActivity={handleAddActivity} />
     </>
   );
